014cfdeda731ad68fd1570bff43d4e286c6e0310
elsa2491 <elsa.touma@gmail.com>
2024-03-20 23:29:40 +0100

    free(tmp)

--- bonus/srcs/main_bonus.c
+++ bonus/srcs/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: eltouma <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 14:46:15 by eltouma           #+#    #+#             */
-/*   Updated: 2024/03/20 23:25:28 by eltouma          ###   ########.fr       */
+/*   Updated: 2024/03/20 23:29:23 by eltouma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,19 +37,21 @@ int	main(int argc, char **argv, char **env)
 		pipex.here_doc = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0755);
 		if (pipex.here_doc == -1)
 			ft_handle_file_error(&argv[1], &pipex);
+		tmp = ft_strjoin(argv[2], "\n");
 		while (1)
 		{
 			ft_putstr_fd("> ", 0);
 			line = get_next_line(0);
-			tmp = ft_strjoin(argv[2], "\n");
 			if (!ft_strcmp(line, tmp))
 			{
 				free(line);
+				free(tmp);
 				break ;
 			}
 			ft_putstr_fd(line, pipex.here_doc);
 			free(line);
 		}
+		free(tmp);
 		if (close(pipex.here_doc) == -1)
 			ft_handle_file_error(&argv[1], &pipex);
 		pipex.here_doc = open(argv[1], O_RDONLY, 0755);
